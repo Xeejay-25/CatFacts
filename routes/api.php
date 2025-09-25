b10d29c52f8100ca4d065258a088a48d642fa69a
Carl <cjcefren00243@usep.edu.ph>
2025-09-25 14:30:25 +0800

    Add user management routes for stats, leaderboard, and user details

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CatFactController;
 use App\Http\Controllers\Api\GameController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// User Management Routes
+Route::prefix('users')->group(function () {
+    Route::get('stats', [UserController::class, 'stats']);
+    Route::get('leaderboard', [UserController::class, 'leaderboard']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('{user}', [UserController::class, 'show']);
 });
 
 // Cat Facts API Routes
